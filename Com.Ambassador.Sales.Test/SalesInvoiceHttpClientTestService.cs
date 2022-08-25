ee8bf0146d1d7ff9f6f514db43bc533448ca7f4d
achensetyawati <achenzetya@gmail.com>
2022-08-25 11:07:16 +0700

    update unit test and pdf

--- Com.Ambassador.Sales.Test/SalesInvoiceHttpClientTestService.cs
+++ Com.Ambassador.Sales.Test/SalesInvoiceHttpClientTestService.cs
@@ -32,7 +32,7 @@ namespace Com.Ambassador.Sales.Test
             return Task.Run(() => new HttpResponseMessage()
             {
                 StatusCode = System.Net.HttpStatusCode.OK,
-                Content = new StringContent("{data:{'data':'data', 'Buyers':{'Id':1}, 'Address' :'ad', 'BankAddress':'ad', 'SwiftCode':'ad', 'BankName':'ad', 'Currency' : {'Id':1}}}")
+                Content = new StringContent("{data:{'data':'data', 'Buyers':{'Id':1}, 'Address' :'ad', 'BankAddress':'ad', 'SwiftCode':'ad', 'BankName':'ad', 'Currency' : {'Id':1, Code:'aa'}, 'Type':'local'}}")
             });
         }
 
