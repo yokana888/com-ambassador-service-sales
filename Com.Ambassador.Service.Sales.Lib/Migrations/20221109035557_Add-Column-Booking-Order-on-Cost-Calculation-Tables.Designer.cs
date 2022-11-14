c76bb1a999903ae85392e262d414a44fdbadc7bf
AndreaZain <andrea.kinan@gmail.com>
2022-11-14 09:02:21 +0700

    enhance-cost-calculation-prd
    Add booking order loader

--- Com.Ambassador.Service.Sales.Lib/Migrations/20221109035557_Add-Column-Booking-Order-on-Cost-Calculation-Tables.Designer.cs
+++ Com.Ambassador.Service.Sales.Lib/Migrations/20221109035557_Add-Column-Booking-Order-on-Cost-Calculation-Tables.Designer.cs
@@ -4,14 +4,16 @@ using Com.Ambassador.Service.Sales.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Ambassador.Service.Sales.Lib.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221109035557_Add-Column-Booking-Order-on-Cost-Calculation-Tables")]
+    partial class AddColumnBookingOrderonCostCalculationTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Com.Ambassador.Service.Sales.Lib/Migrations/20221109035557_Add-Column-Booking-Order-on-Cost-Calculation-Tables.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Ambassador.Service.Sales.Lib.Migrations
+{
+    public partial class AddColumnBookingOrderonCostCalculationTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "CCQuantity",
+                table: "GarmentBookingOrderItems",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BOQuantity",
+                table: "CostCalculationGarments",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookingOrderId",
+                table: "CostCalculationGarments",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookingOrderItemId",
+                table: "CostCalculationGarments",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BookingOrderNo",
+                table: "CostCalculationGarments",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CCQuantity",
+                table: "GarmentBookingOrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "BOQuantity",
+                table: "CostCalculationGarments");
+
+            migrationBuilder.DropColumn(
+                name: "BookingOrderId",
+                table: "CostCalculationGarments");
+
+            migrationBuilder.DropColumn(
+                name: "BookingOrderItemId",
+                table: "CostCalculationGarments");
+
+            migrationBuilder.DropColumn(
+                name: "BookingOrderNo",
+                table: "CostCalculationGarments");
+        }
+    }
+}
