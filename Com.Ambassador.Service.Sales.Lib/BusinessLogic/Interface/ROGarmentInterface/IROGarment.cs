4cd004681121e60908fb676c8654ef4371a5f513
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-06-04 09:48:31 +0700

    2024/12/Enhance/Add-Reject-ROGarment

--- Com.Ambassador.Service.Sales.Lib/BusinessLogic/Interface/ROGarmentInterface/IROGarment.cs
+++ Com.Ambassador.Service.Sales.Lib/BusinessLogic/Interface/ROGarmentInterface/IROGarment.cs
@@ -1,5 +1,6 @@
 ﻿using Com.Ambassador.Service.Sales.Lib.Models.ROGarments;
 using Com.Ambassador.Service.Sales.Lib.Utilities.BaseInterface;
+using Com.Ambassador.Service.Sales.Lib.ViewModels.GarmentROViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,5 +12,6 @@ namespace Com.Ambassador.Service.Sales.Lib.BusinessLogic.Interface.ROGarmentInte
     {
         Task<int> PostRO(List<long> listId);
         Task<int> UnpostRO(long id);
+        Task<int> RejectSample(int id, RO_GarmentViewModel viewModel);
     }
 }
--- Com.Ambassador.Service.Sales.Lib/Models/ROGarments/RO_Garment.cs
+++ Com.Ambassador.Service.Sales.Lib/Models/ROGarments/RO_Garment.cs
@@ -31,5 +31,7 @@ namespace Com.Ambassador.Service.Sales.Lib.Models.ROGarments
         public string DocumentsFileName { get; set; }
 
         public bool IsPosted { get; set; }
+        public bool IsRejected { get; set; }
+        public string RejectReason { get; set; }
     }
 }
